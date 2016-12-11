2d88eb43989cceaed9457dd1f5a9f310631605ea
Alex Kolchinski <y.a.kolchinski@gmail.com>
2016-12-10 17:21:39 -0800

    Implemented synthetic data generator

--- matlab/data_sets/getSyntheticData.m
+++ matlab/data_sets/getSyntheticData.m
@@ -1,21 +1,56 @@
-function [ answers, concepts, validationAnswers, validationConcepts ] = getSyntheticData( )
+function [ answers, concepts, validationAnswers, validationConcepts ] = ...
+  getSyntheticData( )
 %getSyntheticData return a set of synthetic student answers
 %   Generated according to the IRT model
-  numStudents = 1000;
+
+
+  function p = pRight(diff, skill)
+    p = .25 + .75/(1.0 + exp(diff - skill));
+  end
+
+  numStudents = 20;
   validationRatio = .1;
   validationSize = numStudents * validationRatio;
-  numConcepts = 10;
-  meanSeqLength = 20;
+  numConcepts = 2;
+  meanQuestionsPerConcept = 20;
+  numQuestions = meanQuestionsPerConcept * numConcepts;
+  pT = 0.1; %chance of learning a concept at each question of that concept
   
-  %X and C are just random junk - make them actual IRT data!
-  X = randi(2,numStudents,numConcepts*meanSeqLength) - 1;
-  C = randi(numConcepts, numStudents, numConcepts*meanSeqLength);
+  %set concept difficulties randomly (iid standard normal)
+  conceptDiffs = normrnd(0,1,numConcepts,1);
+
+  %which concept each question belongs to
+  qConcepts = randi(numConcepts, numQuestions, 1);
+  %make questions within a concept vary by difficulty a bit
+  qDiffs = conceptDiffs(qConcepts) + normrnd(0,.5, numQuestions, 1);
   
+  %Here, each student answers the same questions, so our concepts matrix is
+  %just the same row of question concepts over and over, repeated as many
+  %times as there are students
+  C = repmat(qConcepts', numStudents, 1);
+  
+  X = zeros(numStudents, numQuestions);
+  
+  for i = 1:numStudents
+    %knowledge score per-concept, initialized iid standard normal for prior
+    conceptKnow = normrnd(-2,1,numConcepts,1);
+    for j = 1:numQuestions
+      curConcept = C(j);
+      knowledge = conceptKnow(curConcept);
+      gotItRight = binornd(1, pRight(qDiffs(j), knowledge));
+      X(i,j) = gotItRight;
+      if binornd(1,pT)
+        conceptKnow(curConcept) = knowledge + 2;
+      end
+    end
+  end
+  
+  
+  %split data into training and validation sets
+  %according to the validationRatio
   validationIndices = randperm(numStudents, validationSize);
-  
   validationAnswers = X(validationIndices,:);
   validationConcepts = C(validationIndices,:);
-  
   nonValidationIndices = setdiff(1:numStudents, validationIndices);
   answers = X(nonValidationIndices, :);
   concepts = C(nonValidationIndices, :);
